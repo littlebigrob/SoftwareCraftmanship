0fe7256b548f92da731f98d02b9ef6db69123dd3
littlebigrob <robertcolin88@yahoo.com>
2016-09-08 22:26:55 -0400

    Add files via upload

---
+++ tester.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public class tester {
+
+	public static void test(){
+		 List<Integer> listA=Arrays.asList(1,2,4,5);
+		 List<Integer> listB=Arrays.asList(1,2,3,4);
+		 List<Integer> listC=Arrays.asList();
+		 PrefixComparator cmp=new PrefixComparator();
+		 List<Integer> returnPrefix=Prefix.longestPrefix(listA,listB, cmp );
+		 System.out.print(returnPrefix);
+		 returnPrefix=Prefix.longestPrefix(listA,listA, cmp );
+		 System.out.print(returnPrefix);
+		 returnPrefix=Prefix.longestPrefix(listA,listC, cmp );
+		 System.out.print(returnPrefix);
+		 returnPrefix=Prefix.longestPrefix(listC,listC, cmp );
+		 System.out.print(returnPrefix);
+	}
+}

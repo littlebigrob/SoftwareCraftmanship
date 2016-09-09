c2a06ecb2e1f929f90706706f6169249ecf695b3
littlebigrob <robertcolin88@yahoo.com>
2016-09-08 22:41:28 -0400

    Add files via upload

---
+++ Project1/Prefix.java
@@ -0,0 +1,30 @@
+//this project was made by Robert Milne
+import java.util.*;
+
+
+public class Prefix {
+	
+	
+	 public static void main(String[] args){
+		 tester.test();
+	}
+	
+	public static <T> List<T> longestPrefix(List<T> a, List<T> b, Comparator<? super T> cmp){
+		List<T> returnList=new ArrayList<>();
+		Iterator <T> iteratorA=a.iterator();
+		Iterator <T> iteratorB=b.iterator();
+		T elementA;
+		T elementB;
+		while (iteratorA.hasNext()&&iteratorB.hasNext()){
+			elementA=iteratorA.next();
+			elementB=iteratorB.next();
+			if(cmp.compare(elementA, elementB)==0){
+				returnList.add(elementA);
+			}
+			else{
+				break;
+			}
+		}
+		return returnList;
+	}	
+}
